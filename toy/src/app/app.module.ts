9cc98a2c03dd507ac4ad5e8f8a7db63218acf56f
Shailesh <140874514+Shailesh2103@users.noreply.github.com>
2025-08-24 16:05:25 +0530

    header Footer and Home Page design

--- toy/src/app/app.module.ts
+++ toy/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
